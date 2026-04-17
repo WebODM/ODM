4234fcd348f81c558e6a20a66dc7679b6ffcfa0b
Piero Toffanin <pt@masseranolabs.com>
2026-04-17 11:44:00 -0400

    Do not append existing PATH

--- win32env.bat
+++ win32env.bat
@@ -38,7 +38,7 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set PATH=%VIRTUAL_ENV%\Scripts
 
 :END
 if defined _OLD_CODEPAGE (
